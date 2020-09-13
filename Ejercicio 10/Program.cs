0d78f5a4eadff444734685108ca927bf10d39a7e
eve-lab2020 <evelina.aguirre@hotmail.com>
2020-09-13 02:52:35 -0300

    commit

--- Ejercicio 10/Program.cs
+++ Ejercicio 10/Program.cs
@@ -10,6 +10,33 @@ namespace Ejercicio_10
     {
         static void Main(string[] args)
         {
+            Console.Title = "Ejercicio Nro 10";
+
+            int altura;
+            string asterisco = "*";
+            string espacio = " ";
+            int indice;
+         
+            Console.WriteLine("Ingrese altura: ");
+            while (int.TryParse(Console.ReadLine(), out altura) == false)
+            {
+                Console.WriteLine("ERROR. Ingrese altura: ");
+            }
+
+            for (int i = altura, z = 1; i > 0; i--, z += 2)
+            {
+                for (int g = i; g >= 0; g--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < z; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write("\n");
+            }
+
+            Console.ReadKey();
         }
     }
 }
